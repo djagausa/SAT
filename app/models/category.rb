c739a521fcb6f8909260f17ad94bfad6082961e8
djagausa <djagausa@gmail.com>
2013-11-15 15:25:46 -0500

    Home cat selection completed

--- app/models/category.rb
+++ app/models/category.rb
@@ -6,4 +6,7 @@ class Category < ActiveRecord::Base
   	has_many :shoppers, through: :categorizations
 
 	validates :name, presence: true, uniqueness: true
+
+	scope :all_in_order, -> {all.order(name: :asc)}
+	
 end
