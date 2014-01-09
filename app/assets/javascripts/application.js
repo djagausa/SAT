7ce7883d50d8ce7e6f560c7ac7153772a97f183f
djagausa <djagausa@gmail.com>
2014-01-09 15:28:19 -0500

    heroku precompile:assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.ui.datepicker
+//= require jquery.ui.all
 //= require twitter/bootstrap
 //= require bootstrap-fileupload
 //= require underscore
